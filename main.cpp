281bed16f2f0f26f063e00788c4af4d0f5936073
cclin <cclinet@outlook.com>
2025-02-07 12:25:20 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include "permutation.hpp"
 #include <atomic>
 #include <iostream>
+#include <mutex>
 #include <vector>
 // 使用示例
 int main() {
   try {
-    std::atomic_uint64_t count{0};
-    const auto processor_func = [&count](std::vector<int>) {
-      count.fetch_add(1, std::memory_order_release);
+    // std::atomic_uint64_t count{0};
+    // const auto processor_func = [&count](std::vector<int>) {
+    //   count.fetch_add(1, std::memory_order_release);
+    // };
+    std::mutex mutex;
+    const auto processor_func = [&mutex](std::vector<int> vec) {
+      std::lock_guard lk(mutex);
+      for (const auto &i : vec) {
+        std::cout << i + 1 << ' ';
+      }
+      std::cout << '\n';
     };
-    int n = 24, k = 6;
+    int n = 4, k = 2;
     PermutationProcessor<true> processor(n, k, processor_func);
 
     auto start = std::chrono::high_resolution_clock::now();
     processor.process_all();
     auto end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Processed combinations: " << count << std::endl;
+    // std::cout << "Processed combinations: " << count << std::endl;
     std::cout << "Time taken: "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end -
                                                                        start)
